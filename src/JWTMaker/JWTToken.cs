806705c0027cceec0b41c51dd033af96e6f7da26
PeterBernhardt <peter.bernhardt@relayhealth.com>
2017-02-07 18:46:08 -0800

    Initial commit

---
+++ src/JWTMaker/JWTToken.cs
@@ -0,0 +1,168 @@
+﻿namespace JWTMaker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using System.Text;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public enum JwtHashAlgorithm
+    {
+        RS256,
+        HS384,
+        HS512
+    }
+
+    public class CustomJWTToken
+    {
+        public Dictionary<JwtHashAlgorithm, Func<byte[], byte[], byte[]>> HashAlgorithms;
+
+        public CustomJWTToken()
+        {
+            HashAlgorithms = new Dictionary<JwtHashAlgorithm, Func<byte[], byte[], byte[]>>
+            {
+                {
+                    JwtHashAlgorithm.RS256, (key, value) =>
+                                            {
+                                                using (var sha = new HMACSHA256(key))
+                                                {
+                                                    return sha.ComputeHash(value);
+                                                }
+                                            }
+                },
+                {
+                    JwtHashAlgorithm.HS384, (key, value) =>
+                                            {
+                                                using (var sha = new HMACSHA384(key))
+                                                {
+                                                    return sha.ComputeHash(value);
+                                                }
+                                            }
+                },
+                {
+                    JwtHashAlgorithm.HS512, (key, value) =>
+                                            {
+                                                using (var sha = new HMACSHA512(key))
+                                                {
+                                                    return sha.ComputeHash(value);
+                                                }
+                                            }
+                }
+            };
+        }
+
+        public string Encode(object payload, string key, JwtHashAlgorithm algorithm)
+        {
+            return Encode(payload, Encoding.UTF8.GetBytes(key), algorithm);
+        }
+
+        public string Encode(object payload, byte[] keyBytes, JwtHashAlgorithm algorithm)
+        {
+            var segments = new List<string>();
+            var header = new {alg = algorithm.ToString(), typ = "JWT"};
+
+            var headerBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(header, Formatting.None));
+            var payloadBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload, Formatting.None));
+
+            segments.Add(Base64UrlEncode(headerBytes));
+            segments.Add(Base64UrlEncode(payloadBytes));
+
+            var stringToSign = string.Join(".", segments.ToArray());
+
+            var bytesToSign = Encoding.UTF8.GetBytes(stringToSign);
+
+            var signature = HashAlgorithms[algorithm](keyBytes, bytesToSign);
+            segments.Add(Base64UrlEncode(signature));
+
+            return string.Join(".", segments.ToArray());
+        }
+
+        public string Decode(string token, string key)
+        {
+            return Decode(token, key, true);
+        }
+
+        public string Decode(string token, byte[] keyBytes, bool verify)
+        {
+            var parts = token.Split('.');
+            var header = parts[0];
+            var payload = parts[1];
+            var crypto = Base64UrlDecode(parts[2]);
+
+            var headerJson = Encoding.UTF8.GetString(Base64UrlDecode(header));
+            var headerData = JObject.Parse(headerJson);
+            var payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
+            var payloadData = JObject.Parse(payloadJson);
+
+            if (verify)
+            {
+                var bytesToSign = Encoding.UTF8.GetBytes(string.Concat(header, ".", payload));
+                var algorithm = (string) headerData["alg"];
+
+                var signature = HashAlgorithms[GetHashAlgorithm(algorithm)](keyBytes, bytesToSign);
+                var decodedCrypto = Convert.ToBase64String(crypto);
+                var decodedSignature = Convert.ToBase64String(signature);
+
+                if (decodedCrypto != decodedSignature)
+                    throw new ApplicationException($"Invalid signature. Expected {decodedCrypto} got {decodedSignature}");
+            }
+
+            return payloadData.ToString();
+        }
+
+        public string Decode(string token, string key, bool verify)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            return Decode(token, keyBytes, verify);
+        }
+
+        private JwtHashAlgorithm GetHashAlgorithm(string algorithm)
+        {
+            switch (algorithm)
+            {
+                case "RS256":
+                    return JwtHashAlgorithm.RS256;
+                case "HS384":
+                    return JwtHashAlgorithm.HS384;
+                case "HS512":
+                    return JwtHashAlgorithm.HS512;
+                default:
+                    throw new InvalidOperationException("Algorithm not supported.");
+            }
+        }
+
+        // from JWT spec
+        private string Base64UrlEncode(byte[] input)
+        {
+            var output = Convert.ToBase64String(input);
+            output = output.Split('=')[0]; // Remove any trailing '='s
+            output = output.Replace('+', '-'); // 62nd char of encoding
+            output = output.Replace('/', '_'); // 63rd char of encoding
+            return output;
+        }
+
+        // from JWT spec
+        private byte[] Base64UrlDecode(string input)
+        {
+            var output = input;
+            output = output.Replace('-', '+'); // 62nd char of encoding
+            output = output.Replace('_', '/'); // 63rd char of encoding
+            switch (output.Length%4) // Pad with trailing '='s
+            {
+                case 0:
+                    break; // No pad chars in this case
+                case 2:
+                    output += "==";
+                    break; // Two pad chars
+                case 3:
+                    output += "=";
+                    break; // One pad char
+                default:
+                    throw new Exception("Illegal base64url string!");
+            }
+            var converted = Convert.FromBase64String(output); // Standard base64 decoder
+            return converted;
+        }
+    }
+}
---
+++ src/JWTMaker/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿namespace JWTMaker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Windows;
+    using Jose;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void createTokenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (optionCustom.IsChecked.HasValue && optionCustom.IsChecked.Value)
+                createCustom();
+            else
+                createWithJwt();
+        }
+
+        private void createCustom()
+        {
+            var token = new CustomJWTToken();
+            var payload = new
+            {
+                iss = "urn:ascension.org",
+                sub = userId.Text,
+                patient = patientId.Text,
+                scope = "patient/*.read",
+                aud = "urn:relayhealth.com",
+                exp = DateTime.Now.AddMinutes(8).ToUniversalTime(),
+                iat = DateTime.Now.ToUniversalTime()
+            };
+            // Use private key to sign token
+            var signingCert = new X509Certificate2(@"SigningCert.pfx", "relayhealth");
+            var secretKey = signingCert.Export(X509ContentType.Cert);
+
+            var encodedToken = token.Encode(payload, secretKey, JwtHashAlgorithm.HS512);
+            encodedTokenText.Text = encodedToken;
+
+            // Now decode and validate with public kdey
+            var validatingCert = new X509Certificate2("ValidatingCert.cer");
+            var validatingKey = validatingCert.Export(X509ContentType.Cert);
+            var decodedToken = token.Decode(encodedToken, validatingKey, true);
+
+            var jt = JToken.Parse(decodedToken);
+            var formattedJson = jt.ToString();
+            decodedTokenText.Text = formattedJson;
+        }
+
+        private void createWithJwt()
+        {
+            var payload = new Dictionary<string, object>
+            {
+                {"iss", "urn:ascension.org"},
+                {"sub", userId.Text},
+                {"patient", patientId.Text},
+                {"scope", "patient/*.read"},
+                {"aud", "urn:relayhealth.com"},
+                {"exp", DateTime.Now.AddMinutes(8).ToUniversalTime().ToString(CultureInfo.InvariantCulture)},
+                {"iat", DateTime.Now.ToUniversalTime().ToString(CultureInfo.InvariantCulture)}
+            };
+            // Use private key to sign token
+            var signingCert = new X509Certificate2(@"SigningCert.pfx", "relayhealth");
+            var secretKey = signingCert.Export(X509ContentType.Cert);
+
+            var encodedToken = JWT.Encode(payload, secretKey, JwsAlgorithm.HS256);
+            encodedTokenText.Text = encodedToken;
+
+            // Now decode and validate with public kdey
+            var validatingCert = new X509Certificate2("ValidatingCert.cer");
+            var validatingKey = validatingCert.Export(X509ContentType.Cert);
+            var decodedToken = JWT.Decode(encodedToken, validatingKey);
+
+            var jt = JToken.Parse(decodedToken);
+            var formattedJson = jt.ToString();
+            decodedTokenText.Text = formattedJson;
+        }
+    }
+}
